27ea5be9211749b240ec070c7e113d45dfa9a531
Alex Devero <deveroalex@gmail.com>
2017-08-30 09:42:33 +0200

    Change name and port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome ALEX');
-}).listen(8080, '127.0.0.1');
+  res.end('Welcome User');
+}).listen(1337, '127.0.0.1');
 
-console.log('Sever running at http://127.0.0.1:8080');
+console.log('Sever running at http://127.0.0.1:1337');
